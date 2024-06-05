dd2f424fe0492060f7eb6c42059a8a8135e660c4
linggash <sempanalingga@gmail.com>
2024-06-05 14:24:11 +0700

    learned about primary foo

---
+++ src/main/java/linggash/spring/core/PrimaryConfiguration.java
@@ -0,0 +1,22 @@
+package linggash.spring.core;
+
+import linggash.spring.core.data.Foo;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class PrimaryConfiguration {
+
+    @Primary
+    @Bean
+    public Foo foo1() {
+        return new Foo();
+    }
+
+    @Bean
+    public Foo foo2() {
+        return new Foo();
+    }
+
+}

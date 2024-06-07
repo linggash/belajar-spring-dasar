c9c1186bcd17ce46ddabc61247526a3c358d160a
linggash <sempanalingga@gmail.com>
2024-06-07 14:16:32 +0700

    learned about lazy bean

--- src/test/java/linggash/spring/core/DependsOnTest.java
+++ src/test/java/linggash/spring/core/DependsOnTest.java
@@ -1,5 +1,6 @@
 package linggash.spring.core;
 
+import linggash.spring.core.data.Foo;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
@@ -16,6 +17,6 @@ public class DependsOnTest {
 
     @Test
     void testDependsOn() {
-
+        applicationContext.getBean(Foo.class);
     }
 }

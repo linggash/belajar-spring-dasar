8b7d4b6094feda4acd39dd6bdc7512c8a0f1b6f0
linggash <sempanalingga@gmail.com>
2024-06-07 11:14:46 +0700

    learned about depends on

---
+++ src/main/java/linggash/spring/core/DependsOnConfiguration.java
@@ -0,0 +1,29 @@
+package linggash.spring.core;
+
+import linggash.spring.core.data.Bar;
+import linggash.spring.core.data.Foo;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+
+@Slf4j
+@Configuration
+public class DependsOnConfiguration {
+
+    @Bean
+    @DependsOn({
+            "bar"
+    })
+    public Foo foo() {
+        log.info("created new foo");
+        return new Foo();
+    }
+
+    @Bean
+    public Bar bar() {
+        log.info("created new bar");
+        return new Bar();
+    }
+
+}

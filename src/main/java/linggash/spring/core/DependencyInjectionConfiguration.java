85f03fe75ad253f0a1650775659064b187bff101
linggash <sempanalingga@gmail.com>
2024-06-07 10:43:45 +0700

    learned about selecting dependency

--- src/main/java/linggash/spring/core/DependencyInjectionConfiguration.java
+++ src/main/java/linggash/spring/core/DependencyInjectionConfiguration.java
@@ -3,14 +3,22 @@ package linggash.spring.core;
 import linggash.spring.core.data.Bar;
 import linggash.spring.core.data.Foo;
 import linggash.spring.core.data.FooBar;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class DependencyInjectionConfiguration {
 
     @Bean
-    public Foo foo() {
+    @Primary
+    public Foo fooFirst() {
+        return new Foo();
+    }
+
+    @Bean
+    public Foo fooSecond() {
         return new Foo();
     }
 
@@ -19,8 +27,14 @@ public class DependencyInjectionConfiguration {
         return new Bar();
     }
 
+    // jika terdapat bean yang tidak unique maka kita dapat memilihnya secara spesifik
+    // menggunakan annotation @Qualifier("namanya")
+
     @Bean
-    public FooBar fooBar(Foo foo, Bar bar) {
+    public FooBar fooBar(
+            @Qualifier("fooSecond") Foo foo,
+            Bar bar
+    ) {
         return new FooBar(foo, bar);
     }
 

4a29c7666f92bdb0de7fd8c912c4924edc139ee9
linggash <sempanalingga@gmail.com>
2024-08-29 09:03:53 +0700

    Learned about Spring Boot Application

---
+++ src/main/java/linggash/spring/core/application/FooApplication.java
@@ -0,0 +1,24 @@
+package linggash.spring.core.application;
+
+import linggash.spring.core.data.Foo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class FooApplication {
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(FooApplication.class, args);
+
+        Foo foo = applicationContext.getBean(Foo.class);
+        System.out.println(foo);
+    }
+
+    @Bean
+    public Foo foo() {
+        return new Foo();
+    }
+
+}

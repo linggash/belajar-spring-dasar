73bd71090d2852e6c388163a081c58d67a945d7e
linggash <sempanalingga@gmail.com>
2024-08-29 09:18:45 +0700

    Learned about Customizing Spring Application

--- src/main/java/linggash/spring/core/application/FooApplication.java
+++ src/main/java/linggash/spring/core/application/FooApplication.java
@@ -2,6 +2,7 @@ package linggash.spring.core.application;
 
 import linggash.spring.core.data.Bar;
 import linggash.spring.core.data.Foo;
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -10,8 +11,18 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class FooApplication {
 
+//    public static void main(String[] args) {
+//        ConfigurableApplicationContext applicationContext = SpringApplication.run(FooApplication.class, args);
+//
+//        Foo foo = applicationContext.getBean(Foo.class);
+//        System.out.println(foo);
+//    }
+
     public static void main(String[] args) {
-        ConfigurableApplicationContext applicationContext = SpringApplication.run(FooApplication.class, args);
+        SpringApplication application = new SpringApplication(FooApplication.class);
+        application.setBannerMode(Banner.Mode.OFF);
+
+        ConfigurableApplicationContext applicationContext = application.run(args);
 
         Foo foo = applicationContext.getBean(Foo.class);
         System.out.println(foo);

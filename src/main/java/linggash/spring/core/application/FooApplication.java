b9ed2ddf7d85e73fe6ec747c37b06e30bd2c61b4
linggash <sempanalingga@gmail.com>
2024-08-29 14:22:59 +0700

    Learned about Spring Application Event

--- src/main/java/linggash/spring/core/application/FooApplication.java
+++ src/main/java/linggash/spring/core/application/FooApplication.java
@@ -2,11 +2,14 @@ package linggash.spring.core.application;
 
 import linggash.spring.core.data.Bar;
 import linggash.spring.core.data.Foo;
+import linggash.spring.core.listener.AppStartingListener;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class FooApplication {
@@ -21,6 +24,7 @@ public class FooApplication {
     public static void main(String[] args) {
         SpringApplication application = new SpringApplication(FooApplication.class);
         application.setBannerMode(Banner.Mode.OFF);
+        application.setListeners(List.of(new AppStartingListener()));
 
         ConfigurableApplicationContext applicationContext = application.run(args);
 
